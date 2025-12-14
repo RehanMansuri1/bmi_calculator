8387f9c850049fa8c7acbec8b886b4e2d69fa8cc
Md Rehan Mansuri <mansurim975@gmail.com>
2025-12-14 18:57:43 +0530

    Initial commit - BMI Calculator React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
+  const [bmiResult, setBmiresult] = useState('BMI')
+  const [msg , setMsg] = useState('')
+
+  const handleInput = (e) => {
+
+
+    if (e.target.name === 'weight') {
+      setWeight(e.target.value)
+    } else if (e.target.name === 'height') {
+      setHeight(e.target.value)
+    }
+  }
+
+
+  const calculate = () => {
+    //formaula bmi = weight/heght*height(in meter)
+    // 1 feet = .3048
+    let meter = height * 0.3048
+    let meterSquare = meter * meter;
+    let bmi = (weight / meterSquare).toFixed(2);
+    setBmiresult(`Your BMI is : ${bmi}`)
+    if (bmi<18) {
+      setMsg('Hey! you are under weighr')
+    } else if (bmi>=18 && bmi<=25) {
+      setMsg('Hey! you have normal wieght')
+    } else{
+      setMsg('Hey Dude ! you have heavy weight')
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='heading'>
+        <h1>BMI Calculator</h1>
+      </div>
+      <div className='inputs'>
+        <p>Please Enter your Weight (kg)</p>
+        <input type='text' placeholder='Enter weight' name='weight' onChange={handleInput} />
+        <div className='height'>
+          <p>Please Enter your Height (Feet)</p>
+          <input type='text' placeholder='Enter Height' name='height' onChange={handleInput} />
+        </div>
+      </div>
+      <div className='btns'>
+        <button onClick={calculate}>Calculate</button>
+        <button className='reset'>Reset</button>
+      </div>
+      <div className='result'>
+        <p>{bmiResult}</p>
+        <p>{msg}</p>
+      </div>
+
     </div>
   );
 }
